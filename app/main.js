851ef34a3120d7fc789fac5a4a1c5a9ac696735b
Code Panda <hicodepanda@gmail.com>
2016-09-22 11:08:24 +0530

    First component files
    here you can get the my angular2 first component files tutorial

---
+++ app/main.js
@@ -0,0 +1,64 @@
+// import some default angular directives..
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var core_2 = require('@angular/core');
+// ###################################### APP COMPONENT #################################
+// creating main component for this app..
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        core_2.Component({
+            selector: 'my-app',
+            directives: [CricketerComponent],
+            template: "\n       <h1>My Favorite cricketer ....</h1> \n       <cricketer > </cricketer>\n    "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//################################### CRICKETER COMPONENT ################################
+var CricketerComponent = (function () {
+    function CricketerComponent() {
+    }
+    CricketerComponent = __decorate([
+        core_2.Component({
+            selector: 'cricketer',
+            template: "\n        <h2> My Favorite cricketer is Yuvraj .. </h2>\n     "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], CricketerComponent);
+    return CricketerComponent;
+}());
+exports.CricketerComponent = CricketerComponent;
+// initializing ur app..
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            imports: [platform_browser_1.BrowserModule],
+            declarations: [AppComponent, CricketerComponent],
+            bootstrap: [AppComponent]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+// creatint a intance for platform .. 
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
+//# sourceMappingURL=main.js.map
